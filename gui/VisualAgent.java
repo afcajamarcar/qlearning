82b81378a76c819f5a44cb2dbcb4a96d913477ee
afcajamarcar <afcajamarcar@unal.edu.co>
2017-10-30 14:55:13 -0500

    set ball to lowest part on inner square

--- gui/VisualAgent.java
+++ gui/VisualAgent.java
@@ -1,11 +1,12 @@
 package gui;
 
 import processing.core.PApplet;
+import fisica.*;
 
 public class VisualAgent extends PApplet {
 	
 	private double xspeed = 2.5;
-	float xpos, ypos; // ball initial position
+	float xposBall, yposBall; // ball initial position
 	int rad = 20;
 
 
@@ -23,24 +24,41 @@ public class VisualAgent extends PApplet {
 		
 		noStroke();
 		ellipseMode(rad);
-		ypos = height/2;
-		xpos = width/2;
+		yposBall = height/2 + 40;
+		xposBall = width/2;
 		
 		
 	}
 
 	public void draw(){
-		
+		//fill background with black color
 		background(0,0,0);
+		//Create container box
 		noFill();
 		stroke(255);
-		rect(10, 10, width-20, 200);
+		rect(10, 10, width-20, 200); //actual box
 		fill(255);
-		if(xpos < 0-rad) {
-			xpos = width;
+		if(xposBall < 0-rad) {
+			xposBall = width;
 		}
-		xpos =  (float) (xpos + (xspeed*-1));
-		ellipse(xpos,ypos,rad,rad);
+		xposBall =  (float) (xposBall + (xspeed*-1));
+		tint(255, 127);
+		ellipse(xposBall,yposBall,rad,rad);
+		System.out.println(getBallPostition());
 		
+	}
+	
+	public float getxposBall() {
+		return this.xposBall;
+	}
+	
+	public float getyposBall() {
+		return this.yposBall;
+	}
+	
+	public String getBallPostition() {
+		String x = Float.toString(getxposBall());
+		String y = Float.toString(getyposBall());
+		return x + " "+ y;
 	}
 }
